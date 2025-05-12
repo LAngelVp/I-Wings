b00f9ed57227d79989cbc0d2ad982df348121f1e
AngelPerez <neal.ocaffry@gmail.com>
2025-05-11 20:31:47 -0600

    a

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoadingComponent } from './components/loading/loading.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
+    LoadingComponent,
     RouterOutlet,
     HomeComponent
   ],
